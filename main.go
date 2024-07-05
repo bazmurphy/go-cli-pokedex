84ec944bcb10ef0b9b6e17c3fa15b7d82ecd5318
Baz <61154071+bazmurphy@users.noreply.github.com>
2024-07-05 21:13:33 +0100

    map and mapb commands

--- main.go
+++ main.go
@@ -2,14 +2,35 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 )
+
+type Config struct {
+	next     string
+	previous string
+}
 
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*Config) error
+}
+
+type LocationAreaResponse struct {
+	Count    int            `json:"count"`
+	Next     string         `json:"next"`
+	Previous string         `json:"previous"`
+	Results  []LocationArea `json:"results"`
+}
+
+type LocationArea struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
 }
 
 func main() {
@@ -17,6 +38,11 @@ func main() {
 }
 
 func startRepl() {
+	config := &Config{
+		next:     "https://pokeapi.co/api/v2/location-area/",
+		previous: "",
+	}
+
 	reader := bufio.NewScanner(os.Stdin)
 
 	for {
@@ -27,9 +53,9 @@ func startRepl() {
 
 		command, exists := getCommands()[inputText] // (!) this is how to lookup on a map that is returned from a function
 		if exists {
-			err := command.callback()
+			err := command.callback(config)
 			if err != nil {
-				fmt.Println("error: ", err)
+				fmt.Println(err)
 			}
 			continue
 		} else {
@@ -51,10 +77,20 @@ func getCommands() map[string]cliCommand {
 			description: "Exit the Pokedex",
 			callback:    commandExit,
 		},
+		"map": {
+			name:        "map",
+			description: "Display the names of (the next) 20 location areas in the Pokemon world",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Display the previous 20 location areas in the Pokemon world",
+			callback:    commandMapB,
+		},
 	}
 }
 
-func commandHelp() error {
+func commandHelp(config *Config) error {
 	fmt.Println()
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
@@ -66,7 +102,64 @@ func commandHelp() error {
 	return nil
 }
 
-func commandExit() error {
+func commandExit(config *Config) error {
 	os.Exit(0)
+	return nil
+}
+
+func commandMap(config *Config) error {
+	err := getLocationArea(config.next, config)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func commandMapB(config *Config) error {
+	if config.previous == "" {
+		return errors.New("cannot go any further back")
+	}
+
+	err := getLocationArea(config.previous, config)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// TODO (!) i don't like having to pass the config all the way down here...
+// but we need to update the config object with the new values that come back in the response
+func getLocationArea(url string, config *Config) error {
+	response, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+
+	var locationAreaData LocationAreaResponse
+	err = json.Unmarshal(body, &locationAreaData)
+	if err != nil {
+		return err
+	}
+
+	// fmt.Println("DEBUG | count:", locationAreaData.Count)
+	// fmt.Println("DEBUG | next:", locationAreaData.Next)
+	// fmt.Println("DEBUG | previous:", locationAreaData.Previous)
+
+	for _, area := range locationAreaData.Results {
+		fmt.Println(area.Name)
+	}
+
+	// TODO (!) per the above comments
+	config.next = locationAreaData.Next
+	config.previous = locationAreaData.Previous
+
 	return nil
 }
