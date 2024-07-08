f3d74441de45e1b52e99a3b979229b85a4ec30b1
Baz <61154071+bazmurphy@users.noreply.github.com>
2024-07-08 11:13:29 +0100

    adjust cache tests

--- internal/pokecache/cache_test.go
+++ internal/pokecache/cache_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestCacheAddGet(t *testing.T) {
-	cache := NewCache(3 * time.Second)
+	cache := NewCache(500 * time.Millisecond)
 
 	keyOneValue := []byte("value-one")
 	keyTwoValue := []byte("value-two")
@@ -33,18 +33,26 @@ func TestCacheAddGet(t *testing.T) {
 }
 
 func TestCacheReapLoop(t *testing.T) {
-	interval := 100 * time.Millisecond
+	interval := 500 * time.Millisecond
 	cache := NewCache(interval)
 
-	cache.Add("key1", []byte("value-one"))
-	cache.Add("key2", []byte("value-two"))
+	keyOneValue := []byte("value-one")
 
-	time.Sleep(interval + time.Second)
+	cache.Add("key1", keyOneValue)
+
+	value, ok := cache.Get("key1")
+	if !ok {
+		t.Error("key1 should exist in the cache")
+	}
+	if !bytes.Equal(keyOneValue, value) {
+		t.Errorf("expected %v | got %v", keyOneValue, value)
+	}
+
+	// sleep so the reap loop will remove the (expired) entry from the cache
+	wait := 100 * time.Millisecond
+	time.Sleep(interval + wait)
 
 	if _, ok := cache.Get("key1"); ok {
 		t.Error("key1 should have been removed from the cache")
-	}
-	if _, ok := cache.Get("key2"); ok {
-		t.Error("key2 should have been removed from the cache")
 	}
 }
